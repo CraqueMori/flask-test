9fd8cfac24ccf5e8c63f322b93648a59730fad65
luisnether <luisnethertng@gmail.com>
2023-03-13 19:30:10 -0400

    Segundo commit, update_front_End

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, Response, send_from_directory
 import cv2
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 camera = cv2.VideoCapture(0)
 
 def generate_frames():
